e6cb3d1c79f28311f82af53484bc857a9a803e81
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2021-11-22 23:44:24 -0300

    Add project files.

---
+++ Calculadora.Tests/CalculadoraTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Calculadora.Tests
+{
+    public class CalculadoraTests
+    {
+        [TestFixture]
+        public class CalculadoraSimplesTests
+        {
+            //Teste Classe AdicionarDoisNumeros
+            [Test]
+            public void DeveAdicionarDoisNumeros()
+            {
+                //SUT - System Under Test
+                var sut = new CalculadoraSimples();
+
+                var resultado = sut.Adicionar(5, 5);
+
+                Assert.That(resultado, Is.EqualTo(10));
+            }
+
+            //Teste Classe Multiplicar
+            //DoisNumeros
+            [Test]
+        }
+    }
+}
---
+++ Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculadora
+{
+    public class CalculadoraSimples
+    {
+        //Método para adicionar 2 números
+        public int Adicionar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        //Método para multiplicar 2 números
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
